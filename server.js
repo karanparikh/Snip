9f212e2506b0e11e3e0b41a9c27df826f8e39a37
Karan Parikh <karan.painkiller@gmail.com>
2011-08-01 23:50:24 -0700

    removed deps folder. need to work on displaying the code using js syntax highlighter

--- server.js
+++ server.js
@@ -1,8 +1,5 @@
 var express = require('express'),
-    redis = require('redis'),
-    sys = require('sys'),
-    cp = require('child_process'),
-    _ = require('./deps/underscore.js')._;
+    redis = require('redis');
 
 var app = express.createServer();
 
@@ -12,149 +9,14 @@ app.set('view options', {
 });
 app.use(express.bodyParser());
 
-var languages =
- [["apacheconf", "ApacheConf"],
-  ["applescript", "AppleScript"],
-  ["as", "ActionScript"],
-  ["as3", "ActionScript 3"],
-  ["basemake", "Makefile"],
-  ["bash", "Bash"],
-  ["bat", "Batchfile"],
-  ["bbcode", "BBCode"],
-  ["befunge", "Befunge"],
-  ["boo", "Boo"],
-  ["brainfuck", "Brainfuck"],
-  ["c", "C"],
-  ["c-objdump", "c-objdump"],
-  ["cheetah", "Cheetah"],
-  ["clojure", "Clojure"],
-  ["common-lisp", "Common Lisp"],
-  ["control", "Debian Control file"],
-  ["cpp", "C++"],
-  ["cpp-objdump", "cpp-objdump"],
-  ["csharp", "C#"],
-  ["css", "CSS"],
-  ["css+django", "CSS+Django/Jinja"],
-  ["css+erb", "CSS+Ruby"],
-  ["css+genshitext", "CSS+Genshi Text"],
-  ["css+mako", "CSS+Mako"],
-  ["css+mako", "CSS+Mako"],
-  ["css+myghty", "CSS+Myghty"],
-  ["css+php", "CSS+PHP"],
-  ["css+smarty", "CSS+Smarty"],
-  ["d", "D"],
-  ["d-objdump", "d-objdump"],
-  ["delphi", "Delphi"],
-  ["diff", "Diff"],
-  ["django", "Django/Jinja"],
-  ["dpatch", "Darcs Patch"],
-  ["dylan", "Dylan"],
-  ["erb", "ERB"],
-  ["erlang", "Erlang"],
-  ["fortran", "Fortran"],
-  ["gas", "GAS"],
-  ["genshi", "Genshi"],
-  ["genshitext", "Genshi Text"],
-  ["gnuplot", "Gnuplot"],
-  ["groff", "Groff"],
-  ["haskell", "Haskell"],
-  ["html", "HTML"],
-  ["html+cheetah", "HTML+Cheetah"],
-  ["html+django", "HTML+Django/Jinja"],
-  ["html+genshi", "HTML+Genshi"],
-  ["html+mako", "HTML+Mako"],
-  ["html+mako", "HTML+Mako"],
-  ["html+myghty", "HTML+Myghty"],
-  ["html+php", "HTML+PHP"],
-  ["html+smarty", "HTML+Smarty"],
-  ["ini", "INI"],
-  ["io", "Io"],
-  ["irc", "IRC logs"],
-  ["java", "Java"],
-  ["js", "JavaScript"],
-  ["js+cheetah", "JavaScript+Cheetah"],
-  ["js+django", "JavaScript+Django/Jinja"],
-  ["js+erb", "JavaScript+Ruby"],
-  ["js+genshitext", "JavaScript+Genshi Text"],
-  ["js+mako", "JavaScript+Mako"],
-  ["js+mako", "JavaScript+Mako"],
-  ["js+myghty", "JavaScript+Myghty"],
-  ["js+php", "JavaScript+PHP"],
-  ["js+smarty", "JavaScript+Smarty"],
-  ["jsp", "Java Server Page"],
-  ["lhs", "Literate Haskell"],
-  ["lighty", "Lighttpd configuration file"],
-  ["llvm", "LLVM"],
-  ["logtalk", "Logtalk"],
-  ["lua", "Lua"],
-  ["make", "Makefile"],
-  ["mako", "Mako"],
-  ["mako", "Mako"],
-  ["matlab", "Matlab"],
-  ["matlabsession", "Matlab session"],
-  ["minid", "MiniD"],
-  ["moocode", "MOOCode"],
-  ["mupad", "MuPAD"],
-  ["myghty", "Myghty"],
-  ["mysql", "MySQL"],
-  ["nasm", "NASM"],
-  ["nginx", "Nginx configuration file"],
-  ["numpy", "NumPy"],
-  ["objdump", "objdump"],
-  ["objective-c", "Objective-C"],
-  ["ocaml", "OCaml"],
-  ["perl", "Perl"],
-  ["php", "PHP"],
-  ["pot", "Gettext Catalog"],
-  ["pov", "POVRay"],
-  ["py3tb", "Python 3.0 Traceback"],
-  ["pycon", "Python console session"],
-  ["pytb", "Python Traceback"],
-  ["python", "Python"],
-  ["python3", "Python 3"],
-  ["raw", "Raw token data"],
-  ["rb", "Ruby"],
-  ["rbcon", "Ruby irb session"],
-  ["redcode", "Redcode"],
-  ["rhtml", "RHTML"],
-  ["rst", "reStructuredText"],
-  ["scala", "Scala"],
-  ["scheme", "Scheme"],
-  ["smalltalk", "Smalltalk"],
-  ["smarty", "Smarty"],
-  ["sourceslist", "Debian Sourcelist"],
-  ["splus", "S"],
-  ["sql", "SQL"],
-  ["sqlite3", "sqlite3con"],
-  ["squidconf", "SquidConf"],
-  ["tcl", "Tcl"],
-  ["tcsh", "Tcsh"],
-  ["tex", "TeX"],
-  ["text", "Text only"],
-  ["trac-wiki", "MoinMoin/Trac Wiki markup"],
-  ["vb.net", "VB.net"],
-  ["vim", "VimL"],
-  ["xml", "XML"],
-  ["xml+cheetah", "XML+Cheetah"],
-  ["xml+django", "XML+Django/Jinja"],
-  ["xml+erb", "XML+Ruby"],
-  ["xml+mako", "XML+Mako"],
-  ["xml+mako", "XML+Mako"],
-  ["xml+myghty", "XML+Myghty"],
-  ["xml+php", "XML+PHP"],
-  ["xml+smarty", "XML+Smarty"],
-  ["xslt", "XSLT"],
-  ["yaml", "YAML"]];
-
-var genLanguageMap = function() {
-    var result = {};
-    for (var i in languages){
-        result[languages[i][0]] = languages[i][1];
-    }
-    return result;
-}
-
-var language_map= genLanguageMap();
+var language_map = {
+    'cpp': "C++",
+    'java': "Java",
+    'perl': "Perl",
+    'php': "PHP",
+    'python': "Python",
+    'ruby': "Ruby"
+};
 
 app.listen(8080);
 console.log("Server started on port 8080");
@@ -185,6 +47,16 @@ app.get(/\/(\d+)/, function(req, res){
     var id = req.params[0];
     var r = redis.createClient();
     r.on('connect', function(){
-        
+        r.get('snippet:' + id, function(err, data){
+            if(!data){
+                //TODO render 404 / internal server error
+                return;
+            }
+            var obj = JSON.parse(data.toString());
+            var code = obj.code;
+            var language = obj.language;
+            r.quit();
+            res.render('display', {code: code, language: language});
+        });
     });
 });
